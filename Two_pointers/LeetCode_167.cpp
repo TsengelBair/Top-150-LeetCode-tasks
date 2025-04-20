ec366eb6c48958b0ceb744b24a595fe32d40c6a3
Bair <tsengel01@bk.ru>
2025-04-20 13:10:25 +0300

    task 167 solved

--- Two_pointers/LeetCode_167.cpp
+++ Two_pointers/LeetCode_167.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <vector>
 
-// задачу решает, но не проходит тест по времени
-// текущая сложность N * logN (цикл + бинарный поиск)
-
 using std::vector;
 
 int binarySearch(vector<int> &nums, int target, int indexToMiss) {
@@ -12,16 +9,18 @@ int binarySearch(vector<int> &nums, int target, int indexToMiss) {
 
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        if (nums[mid] == target && mid != indexToMiss) {
+        if (mid == indexToMiss) {
+            ++mid;
+        }
+
+        if (nums[mid] == target) {
             return mid;
         } else if (nums[mid] < target) {
             left = mid + 1;
         } else if (nums[mid] > target) {
             right = mid - 1;
-        } else if (mid = indexToMiss) {
-            ++left;
-        } 
-    } 
+        }
+    }
 
     return -1;
 }
@@ -43,14 +42,14 @@ vector<int> twoSum(vector<int>& numbers, int target) {
 
 int main()
 {
-    vector<int> nums = {1, 2, 3, 4, 4, 9, 56, 90};
+
+    std::vector<int> nums = {1, 2, 3, 4, 4};
     int target = 8;
 
     vector<int> res = twoSum(nums, target);
     for (const int elem : res) {
         std::cout << elem << " ";
     }
-    std::cout << std::endl;
 
     return 0;
 }
